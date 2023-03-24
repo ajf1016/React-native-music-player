bd993ba040d6cc1be3067b683680408d7e884226
ajmalfayiz <ajmal.woodenclouds@gmail.com>
2023-03-25 00:04:57 +0530

    latest

--- spotifyConfig.js
+++ spotifyConfig.js
@@ -1,4 +1,4 @@
 const Client_ID = 'a72fa60edd6e42d48a65da3a819468c3';
 var SECRET = '7a0f283797834b9c8bf62f292708fab4'; // Your secret
 var redirect_uri = 'REDIRECT_URI';
-export const BASE_URL = `https://api.spotify.com/v1`;
+export const BASE_URL = 'https://api.spotify.com/v1';
